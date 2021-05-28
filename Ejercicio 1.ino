e35df092227172cd937be3605c740b12a015400b
RocioPavoni <83459543+RocioPavoni@users.noreply.github.com>
2021-05-28 19:24:52 -0300

    Create Ejercicio 1.ino

---
+++ Ejercicio 1.ino
@@ -0,0 +1,22 @@
+void setup(){
+Serial.begin(9600);
+
+}
+
+void loop(){
+
+}
+
+void serialEvent(){
+  if(Serial.available()){
+  Serial.println("Se ingresaron datos");
+    char c = Serial.read();
+  }
+
+
+}
+
+
+
+
+
